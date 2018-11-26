948cc16e8075b88c1c6067399d5921faec2c352b
widakay <erik@yoerik.com>
2018-11-25 23:47:53 -0800

    updated template

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
- 
+import random 
 app = Flask(__name__)
  
  
 @app.route('/')
 def index():
-    return render_template("index.html")
+    now = random.sample(["full","warning","empty"], 1)[0]
+    return render_template("base.html", status=now)
  
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
